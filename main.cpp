e1589794404d55a8124f6ae313ed667636634fd9
Ciprian Spiridon <ciprian.spiridon@gmail.com>
2021-10-02 14:24:49 +0300

    main modification

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <stdio.h>
 
 int main()
 {
     std::cout << "Hello, Darkness, my old friend!" << std::endl;
+    getchar();
+    std::cout << "I've come to talk to you again." << std::endl;
     return 0;
 }
